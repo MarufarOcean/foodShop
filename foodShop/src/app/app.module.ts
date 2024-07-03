3f8b70e06793e9c0ea5ac8141c2b99abda88a525
MarufarOcean <marufarocean@gmail.com>
2024-07-03 14:45:34 +0600

    Fixed issue

--- foodShop/src/app/app.module.ts
+++ foodShop/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { StarRatingModule } from 'angular-star-rating';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StarRatingModule.forRoot()
   ],
   providers: [
     provideClientHydration()
